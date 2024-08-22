d7dc214b3a6a4aff4eb351aaa14b80a2df5de1d3
wellmota <impeto.wellington@gmail.com>
2024-08-22 20:52:45 -0300

    chore: Update form submission functionality and opening hours

--- src/modules/form/submit.js
+++ src/modules/form/submit.js
@@ -1,23 +1,53 @@
-import dayjs from "dayjs";
-import "dayjs/locale/en"; // Import the locale if needed
-const form = document.querySelector("form");
-
-const selectedDate = document.getElementById("date");
+import dayjs from "dayjs"
+import "dayjs/locale/en" // Import the locale if needed
+const form = document.querySelector("form")
+const clientName = document.getElementById("client")
+const selectedDate = document.getElementById("date")
 
 // Today's date
 
-const today = dayjs().format("YYYY-MM-DD");
+const today = dayjs().format("YYYY-MM-DD")
 
 // Load actual date on the date input
 
-selectedDate.value = today;
+selectedDate.value = today
 
 // Define min date on the date input
 
-selectedDate.min = today;
+selectedDate.min = today
 
 form.onsubmit = (event) => {
   // Prevent the default form submission
-  event.preventDefault();
-  console.log("enviado");
-};
+  event.preventDefault()
+
+  try {
+    // return client name
+    const name = clientName.value.trim()
+
+    if (!name) {
+      return alert("Por favor, preencha o campo nome")
+    }
+
+    // get hour
+    const hourSelected = document.querySelector(".hour-selected")
+
+    if (!hourSelected) {
+      return alert("Por favor, selecione um horário")
+    }
+
+    // get only hour
+    const [hour] = hourSelected.innerText.split(":")
+
+    // generate ID
+
+    const id = new Date().getTime()
+
+    // add hour at date
+    const when = dayjs(selectedDate.value).add(hour, "hour")
+    
+    console.log({ id, name, when })
+  } catch (error) {
+    alert("Não foi possível realizar o agendamento")
+    console.error(error)
+  }
+}
