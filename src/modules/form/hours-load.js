ed902336dfab93efdf21fb71a8ba64cdcab5cbfd
wellmota <impeto.wellington@gmail.com>
2024-08-22 17:00:34 -0300

    chore: Update schedules loading and form submission functionality

--- src/modules/form/hours-load.js
+++ src/modules/form/hours-load.js
@@ -1,17 +1,43 @@
 import dayjs from "dayjs"
 import { openingHours } from "../../utils/opening-hours.js"
 
+const hours = document.getElementById("hours")
+
 export function hoursLoad({ date }) {
-  openingHours.map((hour) => {
+  const opening = openingHours.map((hour) => {
     const [scheduleHour] = hour.split(":")
 
     // add hour and verify if is on past
 
-    const isHourPast = dayjs(date).add(scheduleHour, "hour").isAfter(dayjs())
+    const isHourPast = dayjs(date).add(scheduleHour, "hour").isBefore(dayjs())
 
-   return{
+    return {
       hour,
       available: !isHourPast,
     }
   })
+
+  opening.forEach(({ hour, available }) => {
+    const li = document.createElement("li")
+    li.classList.add("hour")
+    li.classList.add(available ? "hour-available" : "hour-unavailable")
+    li.textContent = hour
+
+    if (hour === "9:00") {
+      hourHeaderAdd("Manhã")
+    } else if (hour === "13:00") {
+      hourHeaderAdd("Tarde")
+    } else if (hour === "17:00") {
+      hourHeaderAdd("Noite")
+    }
+
+    hours.append(li)
+  })
+}
+
+function hourHeaderAdd(title) {
+  const header = document.createElement("li")
+  header.classList.add("hour-period")
+  header.textContent = title
+  hours.append(header)
 }
--- src/modules/form/submit.js
+++ src/modules/form/submit.js
@@ -1,22 +1,23 @@
-import dayjs from "dayjs"
-const form = document.querySelector("form")
+import dayjs from "dayjs";
+import "dayjs/locale/en"; // Import the locale if needed
+const form = document.querySelector("form");
 
-const selectedDate = document.getElementById("date")
+const selectedDate = document.getElementById("date");
 
 // Today's date
 
-const today = dayjs(new Date()).format("YYYY-MM-DD")
+const today = dayjs().format("YYYY-MM-DD");
 
 // Load actual date on the date input
 
-selectedDate.value = dayjs(today)
+selectedDate.value = today;
 
 // Define min date on the date input
 
-selectedDate.min = dayjs(new Date()).format(today)
+selectedDate.min = today;
 
 form.onsubmit = (event) => {
   // Prevent the default form submission
-  event.preventDefault()
-  console.log("enviado")
-}
+  event.preventDefault();
+  console.log("enviado");
+};
